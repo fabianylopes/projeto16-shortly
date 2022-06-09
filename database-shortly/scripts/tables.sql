3fe0d049023ce696aaf7a141260492460229bdcd
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-06-09 14:38:10 -0300

    feat: database

--- database-shortly/scripts/tables.sql
+++ database-shortly/scripts/tables.sql
@@ -8,7 +8,9 @@ CREATE TABLE "users" (
 CREATE TABLE "urls" (
   "id" SERIAL PRIMARY KEY,
   "url" TEXT NOT NULL,
-  "userId" INTEGER NOT NULL REFERENCES users(id)
+  "userId" INTEGER NOT NULL REFERENCES users(id),
+  "shortUrl" TEXT NOT NULL UNIQUE,
+	"visitCount" INTEGER NOT NULL DEFAULT '0',
 );
 
 CREATE TABLE "sessions" (
