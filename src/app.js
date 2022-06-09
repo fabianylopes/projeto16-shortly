0eb0da5c1e95efb5ea2f9a31c2e3f8b024baf950
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-06-09 14:39:25 -0300

    fix database connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-import express, { json } from "express";
+import express, { json } from 'express';
+import cors from 'cors';
+import chalk from 'chalk';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,6 +8,7 @@ dotenv.config();
 import router from './routes/index.js';
 
 const app = express();
+app.use(cors());
 app.use(json());
 
 app.use(router);
--- src/db.js
+++ src/db.js
@@ -7,9 +7,6 @@ const { Pool } = pg;
 
 const db = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
 });
 
 export default db;
